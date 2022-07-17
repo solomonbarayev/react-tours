347f3b864190e63a52e463847a3d97a2e33f56ac
solomonbarayev <sbarayev2@gmail.com>
2022-07-17 21:08:34 +0300

    Updates

---
+++ precache-manifest.c3b2971a6ce90a1a36b0a138b62d0f71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a48df5a9aa9cbad966a7b7ab0792d38a",
+    "url": "/react-tours.git/index.html"
+  },
+  {
+    "revision": "ce50f259624211f2dde5",
+    "url": "/react-tours.git/static/css/main.7b074604.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/react-tours.git/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tours.git/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce50f259624211f2dde5",
+    "url": "/react-tours.git/static/js/main.54a2c966.chunk.js"
+  },
+  {
+    "revision": "a3f056c3ac787824705f",
+    "url": "/react-tours.git/static/js/runtime-main.f4c0d0fb.js"
+  }
+]);
